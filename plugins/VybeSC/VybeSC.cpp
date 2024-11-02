fee132bd7700d3ec538074a2caac51649d22aa7a
Paulo Feodrippe <pfeodrippe@gmail.com>
2024-11-02 15:19:18 -0400

    Initial commit

---
+++ plugins/VybeSC/VybeSC.cpp
@@ -0,0 +1,39 @@
+// PluginVybeSC.cpp
+// Paulo Feodrippe (pfeodrippe@gmail.com)
+
+#include "SC_PlugIn.hpp"
+#include "VybeSC.hpp"
+
+static InterfaceTable* ft;
+
+namespace VybeSC {
+
+VybeSC::VybeSC() {
+    mCalcFunc = make_calc_function<VybeSC, &VybeSC::next>();
+    next(1);
+}
+
+void VybeSC::next(int nSamples) {
+
+    // Audio rate input
+    const float* input = in(0);
+
+    // Control rate parameter: gain.
+    const float gain = in0(1);
+
+    // Output buffer
+    float* outbuf = out(0);
+
+    // simple gain function
+    for (int i = 0; i < nSamples; ++i) {
+        outbuf[i] = input[i] * gain;
+    }
+}
+
+} // namespace VybeSC
+
+PluginLoad(VybeSCUGens) {
+    // Plugin magic
+    ft = inTable;
+    registerUnit<VybeSC::VybeSC>(ft, "VybeSC", false);
+}
---
+++ plugins/VybeSC/VybeSC.hpp
@@ -0,0 +1,24 @@
+// PluginVybeSC.hpp
+// Paulo Feodrippe (pfeodrippe@gmail.com)
+
+#pragma once
+
+#include "SC_PlugIn.hpp"
+
+namespace VybeSC {
+
+class VybeSC : public SCUnit {
+public:
+    VybeSC();
+
+    // Destructor
+    // ~VybeSC();
+
+private:
+    // Calc function
+    void next(int nSamples);
+
+    // Member variables
+};
+
+} // namespace VybeSC
